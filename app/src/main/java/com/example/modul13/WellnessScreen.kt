fd87d04081f0acab57808553cd17e46eed0d7293
Vinsensius Arden <zeroxleashgaming@gmail.com>
2025-02-07 15:25:38 +0700

    stp 11

--- app/src/main/java/com/example/modul13/WellnessScreen.kt
+++ app/src/main/java/com/example/modul13/WellnessScreen.kt
@@ -1,13 +1,23 @@
 package com.example.modul13
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
+    }
 }
+
+private fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/com/example/modul13/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.example.modul13
+
+data class WellnessTask(val id: Int, val label: String)
